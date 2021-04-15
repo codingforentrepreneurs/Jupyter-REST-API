3a47ab273fff9aed2fa2612b8196be2bdcca83d5
CFE <hello@teamcfe.com>
2021-04-15 12:24:05 -0700

    15 - Deploy to Heroku

--- build.sh
+++ build.sh
@@ -2,3 +2,9 @@ docker build -f Dockerfile.web -t jupyter-resty .
 
 
 # docker run -it --env PORT=8001 -p 8188:8001 jupyter-resty
+
+# docker run jupyter-resty /bin/bash
+
+# docker tag <img-tag> registry.heroku.com/<heroku-app>/<proccess>
+
+# docker push registry.heroku.com/<heroku-app>/<proccess>
